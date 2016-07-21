d4e2a6e7dd801a0c98ce27ac7aab130148eba4e5
tadaren <ptymaato@yahoo.co.jp>
2016-07-21 17:36:49 +0900

    Add files via upload

---
+++ sortSample.java
@@ -0,0 +1,20 @@
+package javatest;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class sortSample {
+
+	public static void main(String[] args) {
+		System.out.print("数値をスペースで区切って3つ入力");
+		Scanner stdIn = new Scanner(System.in);
+		ArrayList<Integer> list = new ArrayList<Integer>();//リストの作成
+		list.add(stdIn.nextInt());	//リストに入力を追加
+		list.add(stdIn.nextInt());
+		list.add(stdIn.nextInt());
+		System.out.println(list);	//リストを表示
+		list.sort(null);				//リストをソートする
+		System.out.println(list);	//ソートしたリストを表示
+	}
+
+}
